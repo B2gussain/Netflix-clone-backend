e677a2d2e70f71ba9da7788e65ed4534455eb290
B2gussain <bittuggussain@gmail.com>
2024-10-09 12:33:26 +0530

    update

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 app.use('/api/auth', require('./routes/auth'));
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => {
   console.log(`Server is running on port https://localhost:${PORT}`);
 });
